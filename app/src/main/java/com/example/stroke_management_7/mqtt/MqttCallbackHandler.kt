550459c27a0136323bee8a53568caf923a030b2a
Duy.ND210280@sis.hust.edu.vn <Duy.ND210280@sis.hust.edu.vn>
2024-05-24 19:28:37 +0700

    My commit

--- app/src/main/java/com/example/stroke_management_7/mqtt/MqttCallbackHandler.kt
+++ app/src/main/java/com/example/stroke_management_7/mqtt/MqttCallbackHandler.kt
@@ -6,6 +6,11 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.example.stroke_management_7.databinding.ActivityMainBinding
 import com.github.mikephil.charting.data.Entry
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
 import org.eclipse.paho.client.mqttv3.MqttCallback
 import org.eclipse.paho.client.mqttv3.MqttException
@@ -13,6 +18,7 @@ import org.eclipse.paho.client.mqttv3.MqttMessage
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
+const val DELAY_TIME = 1000L
 
 class MqttCallbackHandler(
     private val context: Context,
@@ -42,110 +48,159 @@ class MqttCallbackHandler(
 
     @SuppressLint("SetTextI18n")
     override fun messageArrived(topic: String?, message: MqttMessage?) {
-        try {
-            val timestamp =
-                LocalTime.now().format(DateTimeFormatter.ofPattern("HHmmss")).toFloatOrNull()
-                    ?: return
-            val payload = message?.payload?.toString(Charsets.UTF_8)?.toFloatOrNull() ?: return
-            when (topic) {
-                topicArray[0] -> {
-                    if (heartRateEntries.size == 15) {
-                        heartRateEntries.removeAt(0)
+        CoroutineScope(Dispatchers.Default).launch {
+            try {
+                val timestamp =
+                    LocalTime.now().format(DateTimeFormatter.ofPattern("HHmmss")).toFloatOrNull()
+                        ?: return@launch
+                val payload =
+                    message?.payload?.toString(Charsets.UTF_8)?.toFloatOrNull() ?: return@launch
+                when (topic) {
+                    topicArray[0] -> {
+                        if (heartRateEntries.size <= 1 || (timestamp - heartRateEntries.last().x >= 1)) {
+                            if (heartRateEntries.size == 15) {
+                                heartRateEntries.removeAt(0)
+                            }
+                            heartRateEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[0].postValue(heartRateEntries)
+                                binding.tvCurrentHeartRate.text = "$payload Hz"
+                            }
+                        }
                     }
-                    heartRateEntries.add(Entry(timestamp, payload))
-                    topicLiveData[0].postValue(heartRateEntries)
-                    binding.tvCurrentHeartRate.text = "$payload Hz"
+
+                    topicArray[1] -> {
+                        if (tempEntries.size <= 1 || (timestamp - tempEntries.last().x >= 1)) {
+                            if (tempEntries.size == 15) {
+                                tempEntries.removeAt(0)
+                            }
+                            tempEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[1].postValue(tempEntries)
+                                binding.tvCurrentTemp.text = "$payload oC"
+                            }
+                        }
+                    }
+
+                    topicArray[2] -> {
+                        if (accXEntries.size <= 1 || (timestamp - accXEntries.last().x >= 1)) {
+                            if (accXEntries.size == 15) {
+                                accXEntries.removeAt(0)
+                            }
+                            accXEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[2].postValue(accXEntries)
+                            }
+                        }
+                    }
+
+                    topicArray[3] -> {
+                        if (accYEntries.size <= 1 || (timestamp - accYEntries.last().x >= 1)) {
+                            if (accYEntries.size == 15) {
+                                accYEntries.removeAt(0)
+                            }
+                            accYEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[3].postValue(accYEntries)
+                            }
+                        }
+                    }
+
+                    topicArray[4] -> {
+                        if (accZEntries.size <= 1 || (timestamp - accZEntries.last().x >= 1)) {
+                            if (accZEntries.size == 15) {
+                                accZEntries.removeAt(0)
+                            }
+                            accZEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[4].postValue(accZEntries)
+                            }
+                        }
+                    }
+
+                    topicArray[5] -> {
+                        if (gyroXEntries.size <= 1 || (timestamp - gyroXEntries.last().x >= 1)) {
+                            if (gyroXEntries.size == 15) {
+                                gyroXEntries.removeAt(0)
+                            }
+                            gyroXEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[5].postValue(gyroXEntries)
+                            }
+                        }
+                    }
+
+                    topicArray[6] -> {
+                        if (gyroYEntries.size <= 1 || (timestamp - gyroYEntries.last().x >= 1)) {
+                            if (gyroYEntries.size == 15) {
+                                gyroYEntries.removeAt(0)
+                            }
+                            gyroYEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[6].postValue(gyroYEntries)
+                            }
+                        }
+                    }
+
+                    topicArray[7] -> {
+                        if (gyroZEntries.size <= 1 || (timestamp - gyroZEntries.last().x >= 1)) {
+                            if (gyroZEntries.size == 15) {
+                                gyroZEntries.removeAt(0)
+                            }
+                            gyroZEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[7].postValue(gyroZEntries)
+                            }
+                        }
+                    }
+
+                    topicArray[8] -> {
+                        if (eulerXEntries.size <= 1 || (timestamp - eulerXEntries.last().x >= 1)) {
+                            if (eulerXEntries.size == 15) {
+                                eulerXEntries.removeAt(0)
+                            }
+                            eulerXEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[8].postValue(eulerXEntries)
+                            }
+                        }
+                    }
+
+                    topicArray[9] -> {
+                        if (eulerYEntries.size <= 1 || (timestamp - eulerYEntries.last().x >= 1)) {
+                            if (eulerYEntries.size == 15) {
+                                eulerYEntries.removeAt(0)
+                            }
+                            eulerYEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[9].postValue(eulerYEntries)
+                            }
+                        }
+                    }
+
+                    topicArray[10] -> {
+                        if (eulerZEntries.size <= 1 || (timestamp - eulerZEntries.last().x >= 1)) {
+                            if (eulerZEntries.size == 5) {
+                                eulerZEntries.removeAt(0)
+                            }
+                            eulerZEntries.add(Entry(timestamp, payload))
+                            withContext(Dispatchers.Main) {
+                                topicLiveData[10].postValue(eulerZEntries)
+                            }
+                        }
+                    }
+
+                    else -> {
+                        //Do nothing
+                    }
                 }
 
-                topicArray[1] -> {
-                    if (tempEntries.size == 15) {
-                        tempEntries.removeAt(0)
-                    }
-                    tempEntries.add(Entry(timestamp, payload))
-                    topicLiveData[1].postValue(tempEntries)
-                    binding.tvCurrentTemp.text = "$payload oC"
-                }
+                Log.i(TAG, "Topic: $topic, Timestamp: $timestamp, Payload: $payload")
 
-                topicArray[2] -> {
-                    if (accXEntries.size == 15) {
-                        accXEntries.removeAt(0)
-                    }
-                    accXEntries.add(Entry(timestamp, payload))
-                    topicLiveData[2].postValue(accXEntries)
-                }
-
-                topicArray[3] -> {
-                    if (accYEntries.size == 15) {
-                        accYEntries.removeAt(0)
-                    }
-                    accYEntries.add(Entry(timestamp, payload))
-                    topicLiveData[3].postValue(accYEntries)
-                }
-
-                topicArray[4] -> {
-                    if (accZEntries.size == 15) {
-                        accZEntries.removeAt(0)
-                    }
-                    accZEntries.add(Entry(timestamp, payload))
-                    topicLiveData[4].postValue(accZEntries)
-                }
-
-                topicArray[5] -> {
-                    if (gyroXEntries.size == 15) {
-                        gyroXEntries.removeAt(0)
-                    }
-                    gyroXEntries.add(Entry(timestamp, payload))
-                    topicLiveData[5].postValue(gyroXEntries)
-                }
-
-                topicArray[6] -> {
-                    if (gyroYEntries.size == 15) {
-                        gyroYEntries.removeAt(0)
-                    }
-                    gyroYEntries.add(Entry(timestamp, payload))
-                    topicLiveData[6].postValue(gyroYEntries)
-                }
-
-                topicArray[7] -> {
-                    if (gyroZEntries.size == 15) {
-                        gyroZEntries.removeAt(0)
-                    }
-                    gyroZEntries.add(Entry(timestamp, payload))
-                    topicLiveData[7].postValue(gyroZEntries)
-                }
-
-                topicArray[8] -> {
-                    if (eulerXEntries.size == 15) {
-                        eulerXEntries.removeAt(0)
-                    }
-                    eulerXEntries.add(Entry(timestamp, payload))
-                    topicLiveData[8].postValue(eulerXEntries)
-                }
-
-                topicArray[9] -> {
-                    if (eulerYEntries.size == 15) {
-                        eulerYEntries.removeAt(0)
-                    }
-                    eulerYEntries.add(Entry(timestamp, payload))
-                    topicLiveData[9].postValue(eulerYEntries)
-                }
-
-                topicArray[10] -> {
-                    if (eulerZEntries.size == 5) {
-                        eulerZEntries.removeAt(0)
-                    }
-                    eulerZEntries.add(Entry(timestamp, payload))
-                    topicLiveData[10].postValue(eulerZEntries)
-                }
-
-                else -> {
-                    //Do nothing
-                }
+            } catch (e: MqttException) {
+                e.printStackTrace()
+                Log.e(TAG, "Exception for message arrived")
             }
-
-        } catch (e: MqttException) {
-            e.printStackTrace()
-            Log.e(TAG, "Exception for message arrived")
         }
     }
 
